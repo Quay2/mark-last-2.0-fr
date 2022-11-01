209f2d03c719619950f5a4ed9d2ffe2838ef032f
Jayden Quaynor <JaydenQuaynor@gmail.com>
2022-10-31 23:11:34 -0400

    sml nav edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,18 @@ import { AiOutlineSearch } from 'react-icons/ai';
 const Navbar = () => {
   return (
     <div className="bg-Mblack mx-auto ">
-    <div className='flex'>
+    <div className='flex justify-around items-center'>
       <h1 className="text-[#fff] font-bold text-3xl">
         Maxify TEXH <span className='text-Mgreen'>.</span>
       </h1>
       <nav>
-        <ul className='flex text-White'>
-          <li>home</li>
-          <li>News</li>
-          <li>Blog</li>
+        <ul className='flex font-semibold uppercase underline decoration-Mgreen underline-offset-4 ml-[-5em] text-White'>
+          <li className='mx-28 my-8'>home</li>
+          <li className='mx-28 my-8'>News</li>
+          <li className='mx-28 my-8'>Blog</li>
         </ul>
       </nav>
-      <AiOutlineSearch />
+      <AiOutlineSearch className='text-[1em]'/>
     </div>
   </div>
   )
