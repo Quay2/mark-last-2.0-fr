f9de35f8056c88efb4376e6f9d4a4477f0f87a6c
Jayden Quaynor <JaydenQuaynor@gmail.com>
2022-11-01 23:02:50 -0400

    123

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Mark from '../assets/mark.png'
 const Hero = () => {
   return (
     <div className="bg-Mblack w-full h-[100vh]">
-      <h1 className='text-White'>Changing <span>TECH </span> For The <span>Masses</span> </h1>
+      <h1 className='text-White text-8xl font-[]'>Changing <span>TECH </span> For The <span>Masses</span> </h1>
         <p className="">Here at MAXIFTY are goal is to help fellow devlopers like our selfs to lunch,  Test , and Depoly there ideas  </p>
         <p className="">CLICK HERE TO MAXIFY YOUR IDEAS </p>
         <button className="">Learn More</button>
