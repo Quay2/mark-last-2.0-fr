151d5d65c131f288c72c8dde0d058d6d81a1724b
ash-github <jaydenquaynor@gmail.com>
2022-11-04 23:16:04 +0000

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Mark from '../assets/mark.png'
+import React from "react";
+import Mark from "../assets/mark.png";
 
 const Hero = () => {
   return (
-    <div className="bg-Mblack w-full h-[100vh]">
-      <h1 className='text-White text-8xl font-[]'>Changing <span>TECH </span> For The <span>Masses</span> </h1>
-        <p className="">Here at MAXIFTY are goal is to help fellow devlopers like our selfs to lunch,  Test , and Depoly there ideas  </p>
-        <p className="">CLICK HERE TO MAXIFY YOUR IDEAS </p>
-        <button className="">Learn More</button>
-        <img src={Mark} alt="#" />
+    <div className="bg-Mblack text-White w-full h-[100vh]">
+      <h1 className="text-White text-8xl font-bold">
+        Changing <span className="text-Mgreen">TECH </span> For The{" "}
+        <span className="text-Mgreen">Masses</span>
+      </h1>
+      <p className="">
+        Here at MAXIFTY are goal is to help fellow devlopers like our selfs to
+        lunch, Test , and Depoly there ideas{" "}
+      </p>
+      <p className="">CLICK HERE TO MAXIFY YOUR IDEAS </p>
+      <button className="">Learn More</button>
+      <img className="" src={Mark} alt="#" />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
